e464345ffdd8764e1214ac0e2f08459a1616d770
Arghya Roy <bubun15072006@gmail.com>
2025-09-25 19:55:40 +0530

    basic_profit.py

---
+++ basic_profit.py
@@ -0,0 +1,8 @@
+# Create the cost_price variable
+cost_price = 25
+
+# Create the selling_price variable 
+selling_price = 35
+# Compute the profit and print it
+profit = selling_price - cost_price
+print(profit)
