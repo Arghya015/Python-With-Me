f2aadbe400d76d936333b9821ca279a62a02fafe
Arghya Roy <bubun15072006@gmail.com>
2025-09-25 20:03:54 +0530

    remainder_quotient.py

---
+++ remainder_quotient.py
@@ -0,0 +1,14 @@
+# Create pen_number and student_number variables
+pen_number = 14
+student_number = 3
+
+# Compute the number of pens each student will get and print it
+# Hint: find the quotient
+pens = pen_number//student_number
+
+
+# Compute and print the number of remaining pens
+# Hint: find the remainder
+remainder = pen_number % student_number
+print("Pens for each student:", pens)
+print("Remaining pens:", remainder)
