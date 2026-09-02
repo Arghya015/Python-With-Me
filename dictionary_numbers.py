f22dfd09f9b409f343607103f2d5e7e04d6af9b3
Arghya Roy <bubun15072006@gmail.com>
2026-09-02 21:55:20 +0530

    Create dictionary_numbers.py

---
+++ dictionary_numbers.py
@@ -0,0 +1,9 @@
+mark1 =int(input("Enter first subject number:"))
+mark2 =int(input("Enter second subject number:"))
+mark3 =int(input("Enter third subject number:"))
+result={}
+result.update({"mark1": mark1})
+result.update({"mark2": mark2})
+result.update({"mark3": mark3})
+
+print(result)
